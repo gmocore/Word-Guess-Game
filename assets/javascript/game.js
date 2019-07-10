e181697196d4aed21460b4c2ecd2eee655d89455
Black <gmocore@gmail.com>
2019-07-10 16:07:17 -0600

    added keyCheck and GuessCheck funtions. Updates lives display, if incorrect

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,36 @@
 const wordChoices = ["mario", "luigi", "toad", "yoshi", "koopa"];
 const randomIndex = Math.floor(Math.random() * wordChoices.length);
+const randomWord = document.getElementById("word");
+const currentWord = (randomWord.innerHTML = wordChoices[randomIndex]);
+const lifeCount = document.getElementById("life-count");
+let livesRemaining = 9;
+let guessResult;
 
-let randomWord = document.getElementById("word");
+function keyCheck() {
+  document.addEventListener("keypress", function(event) {
+    let keyPressed = event.key;
+    console.log(keyPressed);
+    if (currentWord.includes(keyPressed)) {
+      guessResult = "correct";
+      console.log("correct");
+    } else {
+      guessResult = "incorrect";
+      console.log("incorrect");
+      guessCheck();
+    }
+  });
+}
 
-randomWord.innerHTML = wordChoices[randomIndex];
+keyCheck();
 
-console.log(randomIndex);
-console.log(wordChoices[randomIndex]);
+function guessCheck() {
+  if (guessResult === "incorrect") {
+    livesRemaining--;
+    console.log(livesRemaining);
+    lifeCount.innerHTML = `${livesRemaining} lives left!`;
+    if (livesRemaining === 0) {
+      console.log("game over");
+      lifeCount.innerHTML = `GAME OVER!`;
+    }
+  }
+}
